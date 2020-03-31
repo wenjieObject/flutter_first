7ca8f772a5ed104b799495752213481824ecf31f
wenjieObject <connyoursis@163.com>
2020-03-31 09:44:00 +0800

    init

---
+++ lib/common/Global.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+// 提供五套可选主题色
+
+const _themes = <MaterialColor>[
+  Colors.blue,
+  Colors.cyan,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+];
+
+class Global{
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,325 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ScaffoldRoute(),
+    );
+  }
+}
+
+class ScaffoldRoute extends StatefulWidget {
+  @override
+  _ScaffoldRouteState createState() => _ScaffoldRouteState();
+}
+
+class _ScaffoldRouteState extends State<ScaffoldRoute> with SingleTickerProviderStateMixin  {
+  int _selectedIndex = 1;
+
+  TabController _tabController; //需要定义一个Controller
+  List tabs = ["新闻", "历史", "图片"];
+
+  @override
+  void initState() {
+    super.initState();
+    //创建Controller
+    var tabController = TabController(length: tabs.length, vsync: this);
+    _tabController = tabController;
+  }
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          //生成Tab菜单
+          bottom: TabBar(
+            controller: _tabController
+            ,tabs: tabs.map((e) => Tab(text: e)).toList()),
+          //导航栏
+          title: Text("App Name"),
+          actions: <Widget>[
+            //导航栏右侧菜单
+            IconButton(icon: Icon(Icons.share), onPressed: () {}),
+          ],
+          leading: Builder(
+            builder: (context) {
+              return IconButton(
+                  icon: Icon(Icons.dashboard, color: Colors.white),
+                  onPressed: () {
+                    //打开抽屉
+                    Scaffold.of(context).openDrawer();
+                  });
+            },
+          )),
+      drawer: MyDrawer(), //抽屉
+      bottomNavigationBar: BottomNavigationBar(
+        // 底部导航
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.business), title: Text('Business')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.school), title: Text('School')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+      floatingActionButton: FloatingActionButton(
+          //悬浮按钮
+          child: Icon(Icons.add),
+          onPressed: _onAdd),
+      body: TabBarView(
+        controller:_tabController,
+        children: [
+          SwitchAndCheckBoxTestRoute(),
+          ImageWidget(),
+          RandomWordsWidget()
+        ]
+        ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onAdd() {}
+}
+
+//
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: MediaQuery.removePadding(
+        context: context,
+        //移除抽屉菜单顶部默认留白
+        removeTop: true,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 38.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: ClipOval(
+                      child: Image.asset(
+                        "imgs/avatar.png",
+                        width: 80,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Wendux",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: const Icon(Icons.add),
+                    title: const Text('Add account'),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.settings),
+                    title: const Text('Manage accounts'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new WillPopScope(
+                onWillPop: () async {
+                  return false;
+                },
+                child: Container(child: Text('123'))),
+            ImageWidget(),
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            RandomWordsWidget(),
+            Echo(text: "hello world"),
+            IconWidget(),
+            SwitchAndCheckBoxTestRoute(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// 开关
+class SwitchAndCheckBoxTestRoute extends StatefulWidget {
+  @override
+  _SwitchAndCheckBoxTestRouteState createState() =>
+      new _SwitchAndCheckBoxTestRouteState();
+}
+
+class _SwitchAndCheckBoxTestRouteState
+    extends State<SwitchAndCheckBoxTestRoute> {
+  bool _switchSelected = true; //维护单选开关状态
+  bool _checkboxSelected = true; //维护复选框状态
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Switch(
+        value: _switchSelected, //当前状态
+        onChanged: (value) {
+          //重新构建页面
+          setState(() {
+            _switchSelected = value;
+          });
+        },
+      ),
+      Text('data'),
+      Checkbox(
+        value: _checkboxSelected,
+        activeColor: Colors.red, //选中时的颜色
+        onChanged: (value) {
+          //print(value);
+          setState(() {
+            _checkboxSelected = value;
+          });
+        },
+      ),
+      Text('data1'),
+    ]);
+  }
+}
+
+//ICON
+class IconWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          Icons.subway,
+          color: Colors.green,
+        ),
+        Icon(
+          Icons.error,
+          color: Colors.green,
+        ),
+        Icon(
+          Icons.fingerprint,
+          color: Colors.green,
+        ),
+      ],
+    );
+  }
+}
+
+//生成随机字符串
+class RandomWordsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Text(wordPair.toString()),
+    );
+  }
+}
+
+//加载图片
+class ImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'images/a_dot_ham.jpg',
+      width: 100,
+    );
+  }
+}
+
+//echo
+class Echo extends StatelessWidget {
+  const Echo({
+    Key key,
+    @required this.text,
+    this.backgroundColor: Colors.grey,
+  }) : super(key: key);
+
+  final String text;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: backgroundColor,
+        child: Text(text),
+      ),
+    );
+  }
+}
